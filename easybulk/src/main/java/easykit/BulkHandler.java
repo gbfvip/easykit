fa00a092c2b80717b264f89cc13c604422e71062
gaobinfang <gaobinfang@cd.tuan800.com>
2018-11-03 15:11:30 +0800

    lack of concurrency, wait for improve

--- easybulk/src/main/java/easykit/BulkHandler.java
+++ easybulk/src/main/java/easykit/BulkHandler.java
@@ -1,4 +1,194 @@
 package easykit;
 
-public class BulkHandler {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+
+public class BulkHandler<T> {
+    private final int bulkActions;
+    private final ScheduledThreadPoolExecutor scheduler;
+    private final ScheduledFuture<?> scheduledFuture;
+    private final LinkedBlockingQueue<T> footprints;
+    private final int maxRetryNum;
+
+    private Consumer<Collection<T>> operation;
+    private Listener<T> listener;
+
+    private volatile boolean closed = false;
+
+    public interface Listener<T> {
+
+        /**
+         * Callback before the bulk is executed.
+         */
+        void beforeBulk(List<T> footprints);
+
+        /**
+         * Callback after a successful execution of bulk request.
+         */
+        void afterBulk(List<T> footprints);
+
+        /**
+         * Callback after a failed execution of bulk request.
+         */
+        void afterBulk(List<T> footprints, Throwable failure);
+    }
+
+    public static class Builder {
+
+        private Consumer operation;
+        private Listener listener;
+
+        private int bulkActions = 1000;//default max 1000 records/flush
+        private long flushInterval = TimeUnit.MILLISECONDS.toMillis(100);//default max 100ms/flush
+        private int maxRetryNum;
+
+        public Builder setListener(Listener listener) {
+            this.listener = listener;
+            return this;
+        }
+
+        public Builder setOperation(Consumer operation) {
+            this.operation = operation;
+            return this;
+        }
+
+        public Builder setBulkActions(int bulkActions) {
+            this.bulkActions = bulkActions;
+            return this;
+        }
+
+        public Builder setFlushInterval(long interval, TimeUnit timeUnit) {
+            this.flushInterval = timeUnit.toMillis(interval);
+            return this;
+        }
+
+        public Builder setRetry(int maxRetryNum) {
+            this.maxRetryNum = maxRetryNum;
+            return this;
+        }
+
+        /**
+         * Builds a new bulk processor.
+         */
+        public <Value> BulkHandler<Value> build() {
+            return new BulkHandler<Value>(operation, listener, bulkActions, flushInterval, maxRetryNum);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private BulkHandler(Consumer<Collection<T>> operation, Listener<T> listener, int bulkActions, long flushInterval, int maxRetryNum) {
+        this.bulkActions = bulkActions;
+
+        this.operation = operation;
+        this.listener = listener;
+        this.footprints = new LinkedBlockingQueue<T>();
+        this.maxRetryNum = maxRetryNum;
+
+        this.scheduler = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(1);
+        this.scheduler.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
+        this.scheduler.setContinueExistingPeriodicTasksAfterShutdownPolicy(false);
+        this.scheduledFuture = this.scheduler.scheduleWithFixedDelay(new Flush(), flushInterval, flushInterval, TimeUnit.MILLISECONDS);
+    }
+
+    public void close() {
+        awaitClose();
+    }
+
+    private synchronized void awaitClose() {
+        if (closed) {
+            return;
+        }
+        closed = true;
+        if (this.scheduledFuture != null) {
+            this.scheduledFuture.cancel(false);
+            this.scheduler.shutdown();
+            try {
+                this.scheduler.awaitTermination(1, TimeUnit.MINUTES);
+            } catch (Exception ignored) {
+            } finally {
+                this.scheduler.shutdownNow();
+            }
+        }
+        if (footprints.size() > 0) {
+            execute();
+        }
+    }
+
+    public BulkHandler add(T request) {
+        internalAdd(request);
+        return this;
+    }
+
+    private void ensureOpen() {
+        if (closed) {
+            throw new IllegalStateException("bulk process already closed");
+        }
+    }
+
+    private synchronized void internalAdd(T request) {
+        ensureOpen();
+        footprints.add(request);
+        executeIfNeeded();
+    }
+
+    private void executeIfNeeded() {
+        ensureOpen();
+        if (!isOverTheLimit()) {
+            return;
+        }
+        execute();
+    }
+
+    private void execute() {
+        int retry = 0;
+        List<T> bulkRecords = new ArrayList<>();
+        this.footprints.drainTo(bulkRecords);
+        boolean needRetry = execute(bulkRecords);
+        while (needRetry && retry++ < this.maxRetryNum) {
+            needRetry = execute(bulkRecords);
+        }
+    }
+
+    private boolean execute(List<T> bulkRecords) {
+        listener.beforeBulk(bulkRecords);
+        boolean operationFinished = false;
+        try {
+            this.operation.accept(bulkRecords);
+            operationFinished = true;
+            listener.afterBulk(bulkRecords);
+            return false;
+        } catch (Exception e) {
+            if (!operationFinished) {
+                listener.afterBulk(bulkRecords, e);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private boolean isOverTheLimit() {
+        return bulkActions != -1 && footprints.size() >= bulkActions;
+    }
+
+    class Flush implements Runnable {
+
+        @Override
+        public void run() {
+            synchronized (BulkHandler.this) {
+                if (closed) {
+                    return;
+                }
+                if (footprints.isEmpty()) {
+                    return;
+                }
+                execute();
+            }
+        }
+    }
 }
--- easybulk/src/test/java/easykit/BulkHandlerTest.java
+++ easybulk/src/test/java/easykit/BulkHandlerTest.java
@@ -1,4 +1,142 @@
 package easykit;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
 public class BulkHandlerTest {
+    private BulkHandler<String> bulkHandler;
+
+    @Before
+    public void init() {
+        if (bulkHandler != null) {
+            bulkHandler.close();
+        }
+    }
+
+    @Test
+    public void testMaxWaitTimeFlush() {
+        bulkHandler = BulkHandler.builder().setBulkActions(100)
+                .setOperation(new Consumer<Collection<String>>() {
+                    @Override
+                    public void accept(Collection<String> strings) {
+                    }
+                })
+                .setListener(new BulkHandler.Listener<String>() {
+                    @Override
+                    public void beforeBulk(List<String> footprints) {
+                        for (String footprint : footprints) {
+                            System.out.println("before " + footprint);
+                        }
+                    }
+
+                    @Override
+                    public void afterBulk(List<String> footprints) {
+                        for (String footprint : footprints) {
+                            System.out.println("after " + footprint);
+                        }
+                    }
+
+                    @Override
+                    public void afterBulk(List<String> footprints, Throwable failure) {
+                        for (String footprint : footprints) {
+                            System.out.println("after throw " + footprint);
+                        }
+                    }
+                })
+                .setFlushInterval(2000, TimeUnit.MILLISECONDS)
+                .setRetry(3)
+                .build();
+        bulkHandler.add("test1");
+        bulkHandler.add("test2");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testMaxCapacityFlush() {
+        bulkHandler = BulkHandler.builder().setBulkActions(2000)
+                .setOperation(new Consumer<Collection<String>>() {
+                    @Override
+                    public void accept(Collection<String> strings) {
+                    }
+                })
+                .setListener(new BulkHandler.Listener<String>() {
+                    @Override
+                    public void beforeBulk(List<String> footprints) {
+                        System.out.println("before " + footprints.size());
+                    }
+
+                    @Override
+                    public void afterBulk(List<String> footprints) {
+                        System.out.println("after " + footprints.size());
+                    }
+
+                    @Override
+                    public void afterBulk(List<String> footprints, Throwable failure) {
+                        System.out.println("after throw " + footprints.size());
+                    }
+                })
+                .setFlushInterval(2000, TimeUnit.MILLISECONDS)
+                .setRetry(3)
+                .build();
+        for (int i = 0; i < 2001; i++) {
+            bulkHandler.add("test" + i);
+        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testRetry() {
+        bulkHandler = BulkHandler.builder().setBulkActions(1)
+                .setOperation(new Consumer<Collection<String>>() {
+                    @Override
+                    public void accept(Collection<String> strings) {
+                        throw new RuntimeException("test exception");
+                    }
+                })
+                .setListener(new BulkHandler.Listener<String>() {
+                    @Override
+                    public void beforeBulk(List<String> footprints) {
+                        for (String footprint : footprints) {
+                            System.out.println("before " + footprint);
+                        }
+                    }
+
+                    @Override
+                    public void afterBulk(List<String> footprints) {
+                        for (String footprint : footprints) {
+                            System.out.println("after " + footprint);
+                        }
+                    }
+
+                    @Override
+                    public void afterBulk(List<String> footprints, Throwable failure) {
+                        for (String footprint : footprints) {
+                            System.out.println("after throw " + footprint);
+                        }
+                    }
+                })
+                .setFlushInterval(2000, TimeUnit.MILLISECONDS)
+                .setRetry(3)
+                .build();
+        bulkHandler.add("test1");
+        bulkHandler.add("test2");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
